dbc6892998d1eaaa1d4a60aa0fb7e4c485a7b1ee
Samuel Rodrigues <samuelaureliogr@gmail.com>
2020-02-18 11:41:09 -0300

    Mensagemd e Bom Dia adicionada

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -4,6 +4,7 @@ namespace projeto_teste {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Ola Mundo!");
+            Console.WriteLine("Bom Dia");
         }
     }
 }
