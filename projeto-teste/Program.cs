88e811fc91844addfe11ad62d4c77c7c5f6c7491
Samuel Rodrigues <samuelaureliogr@gmail.com>
2020-02-18 11:38:05 -0300

    Projeto iniciado, mostrando amenagem Ola Mundo

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -3,7 +3,7 @@
 namespace projeto_teste {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ola Mundo!");
         }
     }
 }
