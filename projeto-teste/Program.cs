56b6fa051a0415a2cfd9d723c95c2260272afe2f
Samuel Rodrigues <samuelaureliogr@gmail.com>
2020-02-20 15:36:31 -0300

    Acrescentada a menagem desejando um bom dia

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -4,8 +4,8 @@ namespace projeto_teste {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Ola Mundo!");
-            Console.WriteLine("Bom Dia");
             Console.WriteLine("Como você está?");
+            Console.WriteLine("Tenha um Bom Dia!");
         }
     }
 }
